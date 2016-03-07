82168eb8da57d0b6b2e96c74710b3fc0d9eaf50b
hoshiba10 <j_come_true@yahoo.co.jp>
2016-03-07 22:43:34 +0000

    fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -37,20 +37,28 @@ class User < ActiveRecord::Base
 
   ## TODO 実装
   def have(item)
+    have_items.create(user_id: item.id)
   end
-
+        
   def unhave(item)
+    have_item = have_items.find_by(user_id: item.id)
+    have_item.destroy if have_item
   end
 
   def have?(item)
+    have_items.include?(item)
   end
 
   def want(item)
+    want_items.create(user_id: item.id)
   end
 
   def unwant(item)
+    want_item = want_items.find_by(user_id: item.id)
+    want_item.destroy if want_item
   end
 
   def want?(item)
+    want_items.include?(item)
   end
 end
